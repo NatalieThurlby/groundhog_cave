146f2651c605d1798612a756697757207633520b
Natalie Thurlby <Natalie.thurlby@bristol.ac.uk>
2020-10-04 14:57:17 +0100

    buggy cave looping

--- main/Main.gd
+++ main/Main.gd
@@ -1,8 +1,14 @@
 extends Node
 
 export (PackedScene) var Collectable
+export (PackedScene) var CaveWalls
+
 var time_left = 1
 var score
+
+var current_wall
+var walls_1
+var walls_2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +22,22 @@ func game_over():
 	
 	get_tree().call_group("collectables", "queue_free")
 	
+func cave_walls_1():
+	walls_1 = CaveWalls.instance()
+	add_child(walls_1)
+	walls_1.show()
+	walls_1.hidden = false
+	walls_1.connect("loop_cave", self, "_on_loop_cave")
+	current_wall = 1
+	
+func cave_walls_2():
+	walls_2 = CaveWalls.instance()
+	add_child(walls_2)
+	walls_2.show()
+	walls_2.hidden = false
+	walls_2.connect("loop_cave", self, "_on_loop_cave")
+	current_wall = 2
+	
 func new_game():
 	score = 0
 	time_left = 10
@@ -25,7 +47,14 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	
-	$CaveWalls.show()
+	cave_walls_1()
+
+func _on_loop_cave():
+	print('in')
+	if current_wall == 1:
+		cave_walls_2()
+	elif current_wall == 2:
+		cave_walls_1()
 	
 func _on_GameTimer_timeout():
 	time_left -= 1
